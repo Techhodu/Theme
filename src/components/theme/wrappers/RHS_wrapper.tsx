d1257ef8ec817effd08038f65f37fad41397c819
Techhodu <itssocialsk@gmail.com>
2025-01-06 17:50:27 +0530

    fix

--- src/components/theme/wrappers/RHS_wrapper.tsx
+++ src/components/theme/wrappers/RHS_wrapper.tsx
@@ -5,9 +5,7 @@ import RHS_2 from '../RHS_2'
 export default function RHS_wrapper() {
   return (
     <div className="col-span-3 flex flex-col gap-3 md:flex-row lg:col-span-1 lg:flex-col">
-         <LHS_1
-                  
-                  />
+        
           <RHS_1   key={1}
                     limit={12}
                     category={"1"}
